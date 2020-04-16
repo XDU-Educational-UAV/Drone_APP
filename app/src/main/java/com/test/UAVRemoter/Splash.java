1821d717cc0fb9c50b639b6f77c5dcdc69329f62
Phillweston <2436559745@qq.com>
2020-04-16 11:47:30 +0800

    Changed code

--- app/src/main/java/com/test/UAVRemoter/Splash.java
+++ app/src/main/java/com/test/UAVRemoter/Splash.java
@@ -2,8 +2,8 @@ package com.test.UAVRemoter;
 
 import android.content.Intent;
 import android.os.Handler;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {

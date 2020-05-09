f3d48530a2ab52074eb61d24624548304c60c436
Phillweston <2436559745@qq.com>
2020-05-10 00:19:32 +0800

    Changed code

--- app/src/main/java/com/test/UAVRemoter/AuxiliaryBar.java
+++ app/src/main/java/com/test/UAVRemoter/AuxiliaryBar.java
@@ -3,7 +3,7 @@ package com.test.UAVRemoter;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
